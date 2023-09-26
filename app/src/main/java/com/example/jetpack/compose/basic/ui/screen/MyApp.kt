5eb957744e77f8a712318de4663a0493ffef16ae
believerabhi <abhi57m@gmail.com>
2023-09-27 00:25:36 +0100

    Done the persisting state in Compose while configuration change

--- app/src/main/java/com/example/jetpack/compose/basic/ui/screen/MyApp.kt
+++ app/src/main/java/com/example/jetpack/compose/basic/ui/screen/MyApp.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -51,7 +52,7 @@ fun Greetings(modifier: Modifier = Modifier,
 
 @Composable
 fun MyApp(modifier: Modifier = Modifier) {
-    var shouldShowOnboarding by remember { mutableStateOf(true) }
+    var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
 
     Surface(modifier) {
         if(shouldShowOnboarding) {
