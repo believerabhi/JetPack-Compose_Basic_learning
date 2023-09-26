61376d58c2b7cab7d9c922a4aab4346c3d46783a
believerabhi <abhi57m@gmail.com>
2023-09-26 22:15:38 +0100

    Done the Reusing composables

--- app/src/main/java/com/example/jetpack/compose/basic/MainActivity.kt
+++ app/src/main/java/com/example/jetpack/compose/basic/MainActivity.kt
@@ -3,13 +3,16 @@ package com.example.jetpack.compose.basic
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.jetpack.compose.basic.ui.theme.JetPackComposeBasicTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,13 +20,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             JetPackComposeBasicTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                MyApp(modifier = Modifier.fillMaxSize())
             }
         }
     }
@@ -31,13 +28,27 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Column {
+        Surface(color = MaterialTheme.colorScheme.primary) {
+            Text(text = "Hello $name!", modifier = Modifier.padding(8.dp))
+        }
+        Text(text = "Hello $name!")
+    }
+}
+
+@Composable
+fun MyApp(modifier: Modifier = Modifier) {
+    Surface(modifier = modifier,
+        color = MaterialTheme.colorScheme.background
+    ) {
+       Greeting(name = "Idiot")
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     JetPackComposeBasicTheme {
-        Greeting("Abhi")
+       MyApp()
     }
 }
