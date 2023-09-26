33c7c7ca783ace2c54029cdbf236d205bdbe4de1
believerabhi <abhi57m@gmail.com>
2023-09-27 00:35:49 +0100

    Done the Animation in Compose

--- app/src/main/java/com/example/jetpack/compose/basic/ui/screen/MyApp.kt
+++ app/src/main/java/com/example/jetpack/compose/basic/ui/screen/MyApp.kt
@@ -1,5 +1,8 @@
 package com.example.jetpack.compose.basic.ui.screen
 
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -12,6 +15,8 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -19,21 +24,26 @@ import com.example.jetpack.compose.basic.ui.theme.JetPackComposeBasicTheme
 
 @Composable
 fun Greeting(name: String) {
-    val expand = remember { mutableStateOf(false) }
-    val extraPadding = if (expand.value) 48.dp else 0.dp
+    var expand by rememberSaveable { mutableStateOf(false) }
+    val extraPadding by animateDpAsState (
+                if (expand) 48.dp else 0.dp,
+                animationSpec = spring(
+                dampingRatio = Spring.DampingRatioMediumBouncy,
+        stiffness = Spring.StiffnessLow)
+    )
 
     Surface(color = MaterialTheme.colorScheme.primary,
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)) {
         Row(modifier = Modifier.padding(24.dp)) {
             Column(modifier = Modifier
                 .weight(1f)
-                .padding(bottom = extraPadding)
+                .padding(bottom = extraPadding.coerceAtLeast(0.dp))
             ) {
                 Text(text = "Hello, ")
                 Text(text = name)
             }
-            ElevatedButton(onClick = { expand.value = !expand.value }) {
-                Text( if(expand.value) "Show Less" else "Show More")
+            ElevatedButton(onClick = { expand = !expand }) {
+                Text( if(expand) "Show Less" else "Show More")
             }
         }
     }
