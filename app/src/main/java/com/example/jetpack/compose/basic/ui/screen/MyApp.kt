fd8be72080d937cce4ac4288703baa5e702bbcef
believerabhi <abhi57m@gmail.com>
2023-09-27 01:13:39 +0100

    Added Icons and refactored the code to replace button by icon

--- app/src/main/java/com/example/jetpack/compose/basic/ui/screen/MyApp.kt
+++ app/src/main/java/com/example/jetpack/compose/basic/ui/screen/MyApp.kt
@@ -1,5 +1,7 @@
 package com.example.jetpack.compose.basic.ui.screen
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
@@ -9,44 +11,77 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.ElevatedButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
+import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.jetpack.compose.basic.ui.theme.JetPackComposeBasicTheme
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Greeting(name: String) {
-    var expand by rememberSaveable { mutableStateOf(false) }
-    val extraPadding by animateDpAsState (
-                if (expand) 48.dp else 0.dp,
-                animationSpec = spring(
-                dampingRatio = Spring.DampingRatioMediumBouncy,
-        stiffness = Spring.StiffnessLow)
-    )
+    Card(
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.primary
+        ),
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+        ) {
+         CardContent(name = name)
+    }
+}
 
-    Surface(color = MaterialTheme.colorScheme.primary,
-        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)) {
-        Row(modifier = Modifier.padding(24.dp)) {
+@Composable
+fun CardContent(name: String) {
+    var expand by rememberSaveable { mutableStateOf(false) }
+//    val extraPadding by animateDpAsState (
+//        if (expand) 48.dp else 0.dp,
+//        animationSpec = spring(
+//            dampingRatio = Spring.DampingRatioMediumBouncy,
+//            stiffness = Spring.StiffnessLow)
+//    )
+
+        Row(
+            modifier = Modifier
+                .padding(12.dp)
+                .animateContentSize(
+                    animationSpec = spring(
+                        dampingRatio = Spring.DampingRatioMediumBouncy,
+                        stiffness = Spring.StiffnessLow
+                    )
+                )
+        ) {
             Column(modifier = Modifier
                 .weight(1f)
-                .padding(bottom = extraPadding.coerceAtLeast(0.dp))
+                .padding(12.dp)
             ) {
                 Text(text = "Hello, ")
-                Text(text = name)
+                Text(text = name,
+                    style = MaterialTheme.typography.headlineMedium.copy(
+                        fontWeight = FontWeight.ExtraBold
+                    ))
+                if (expand) {
+                    Text(
+                        text = ("Composem ipsum color sit lazy " +
+                                "padding theme elit, sed do bouncy. ").repeat(4),
+                    )
+                }
             }
-            ElevatedButton(onClick = { expand = !expand }) {
-                Text( if(expand) "Show Less" else "Show More")
+//            ElevatedButton(onClick = { expand = !expand }) {
+//                Text( if(expand) "Show Less" else "Show More")
+//            }
+            IconButton(onClick = { expand = !expand }) {
+                Icon(imageVector = if(expand) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+                    contentDescription = if (expand) "Show less" else "Show more")
             }
         }
-    }
     //Text(text = "Hello $name!", modifier = Modifier.fillMaxWidth())
 }
 
@@ -84,6 +119,20 @@ fun MyAppPreview() {
 @Preview(showBackground = true, widthDp = 320)
 @Composable
 private fun GreetingsPreview() {
+    JetPackComposeBasicTheme {
+        Greetings()
+    }
+}
+
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "Dark"
+)
+@Preview(showBackground = true, widthDp = 320)
+@Composable
+fun DefaultPreview() {
     JetPackComposeBasicTheme {
         Greetings()
     }
